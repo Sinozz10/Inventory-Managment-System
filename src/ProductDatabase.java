d384867e5f31f88c042aa342547cdbcde726c967
Yassin <YassinKhaled911@gmail.com>
2025-10-18 02:31:47 +0300

    Added Attributes and Constructor

--- src/ProductDatabase.java
+++ src/ProductDatabase.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
 public class ProductDatabase {
     //ProductDatabase
     // (Constructors: 1 Methods: 8)
+
+    //Attributes
+    private ArrayList<Product> records;
+    private final String filename;
+
+    //Constructor
+    public ProductDatabase(String filename){
+        this.filename = filename;
+        this.records = new ArrayList<>();
+    }
+
+
 }

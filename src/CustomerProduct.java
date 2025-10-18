a4b1e35ddc468a38eddf0261a6c258a450e68ee7
AdhamG <adhamgaweesh@gmail.com>
2025-10-18 22:18:32 +0300

    code cleanup

--- src/CustomerProduct.java
+++ src/CustomerProduct.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 import java.time.LocalDate;
 
-/**
- *
- * @author Asus
- */
 public class CustomerProduct {
-    private String customerSSN;
-    private String productID; 
-    private LocalDate purchaseDate;
-    private boolean paid; 
+    private final String customerSSN, productID;
+    private final LocalDate purchaseDate;
+    private boolean paid;
 
     public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
         this.customerSSN = customerSSN;
@@ -33,9 +23,9 @@ public class CustomerProduct {
     public LocalDate getPurchaseDate() {
         return purchaseDate;
     }
-    
+
     public String lineRepresentation(){
-    return customerSSN + "," + productID + "," + purchaseDate + "," + paid ; 
+    return customerSSN + "," + productID + "," + purchaseDate + "," + paid ;
     }
 
     public boolean isPaid() {
@@ -45,30 +35,8 @@ public class CustomerProduct {
     public void setPaid(boolean paid) {
         this.paid = paid;
     }
-    
+
     public String getSearchKey(){
      return customerSSN + "," + productID + "," + purchaseDate;
-    } 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public static void main(String[] args) {
-        System.out.println("wishy");
     }
-    
 }
--- src/CustomerProductDatabase.java
+++ src/CustomerProductDatabase.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
@@ -10,107 +5,82 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Asus
- */
 public class CustomerProductDatabase {
-    private ArrayList<CustomerProduct> records;
-    private String filename;
-    
+    private final ArrayList<CustomerProduct> records = new ArrayList<>();
+    private final String filename;
+
     public CustomerProductDatabase(String filename) {
         this.filename = filename;
     }
-    
-            
-     public ArrayList<CustomerProduct> returnAllRecords(){
-         return records;
-     }       
-            
-       public boolean contains(String key){
-       
-          for(int i=0;i<records.size();i++){
-             if(records.get(i).getSearchKey().equals(key)) {
-              return true;}
-       }   
-          
-         return false;         
-       }    
-            
-    /**
-     *
-     * @param record
-     */
-    public void insertRecord(CustomerProduct record){
-          if(!contains(record.getSearchKey())) {
-            records.add(record); }   
-          else {
-              System.out.println("already exists"); 
-          }
-       }     
-       
-           
-       public void deleteRecord(String key){
-            if(!contains(key)){
-          System.out.println("this customer product object doesnt exist");     
-          }
-            else{
-           for(int i=0;i<records.size();i++){
-             if(records.get(i).getSearchKey().equals(key)) {
-                records.remove(i);
-                  break;}}
-             } }   
-                  
-       
-       public CustomerProduct getRecord(String key){
-          if(!contains(key)){
-          System.out.println("this customer product object doesnt exist"); 
-          }
-             else  {
-               for(int i=0;i<records.size();i++){
-             if(records.get(i).getSearchKey().equals(key)) {
-              return records.get(i);}}}    
-          return null;
-          }
-          
-      public CustomerProduct createRecordFrom(String line){    
-        String[] product=line.split(",");
-          boolean p=Boolean.parseBoolean(product[3]);
-         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-          LocalDate date=LocalDate.parse(product[2], formatter);    
-        CustomerProduct cust =new CustomerProduct(product[0],product[1],date);      
-        cust.setPaid(p);
-       return cust;         
+
+    public ArrayList<CustomerProduct> returnAllRecords() {
+        return records;
+    }
+
+    public boolean contains(String key) {
+        for (int i = 0; i < records.size(); i++) {
+            if (records.get(i).getSearchKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void insertRecord(CustomerProduct record) {
+        if (!contains(record.getSearchKey())) {
+            records.add(record);
+        } else {
+            System.out.println("already exists");
+        }
+    }
+
+    public void deleteRecord(String key) {
+        if (contains(key)) {
+            for (int i = 0; i < records.size(); i++) {
+                if (records.get(i).getSearchKey().equals(key)) {
+                    records.remove(i);
+                    break;
                 }
-          
-    
-      public void readFromFile() throws FileNotFoundException{
-     File f=new File(filename);
-     Scanner scan=new Scanner(f);
-      while(scan.hasNextLine()){
-       CustomerProduct cust= createRecordFrom(scan.nextLine());
-        insertRecord(cust);
-      }
-   }  
-      
-      
-      
-        public void saveToFile(){
-        
-        
-        
-       
-        }   
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       }
+            }
+        } else {
+            System.out.println("this customer product object doesn't exist");
+        }
+    }
+
+    public CustomerProduct getRecord(String key) {
+        if (contains(key)) {
+            for (int i = 0; i < records.size(); i++) {
+                if (records.get(i).getSearchKey().equals(key)) {
+                    return records.get(i);
+                }
+            }
+        } else {
+            System.out.println("this customer product object doesn't exist");
+        }
+        return null;
+    }
+
+    public CustomerProduct createRecordFrom(String line) {
+        String[] product = line.split(",");
+        boolean p = Boolean.parseBoolean(product[3]);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date = LocalDate.parse(product[2], formatter);
+        CustomerProduct cust = new CustomerProduct(product[0], product[1], date);
+        cust.setPaid(p);
+        return cust;
+    }
+
+    public void readFromFile() throws FileNotFoundException {
+        File f = new File(filename);
+        Scanner scan = new Scanner(f);
+        while (scan.hasNextLine()) {
+            CustomerProduct cust = createRecordFrom(scan.nextLine());
+            insertRecord(cust);
+        }
+    }
+
+    public void saveToFile() {
+    }
+}
             
             

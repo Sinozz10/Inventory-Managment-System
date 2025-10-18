3099b1f19f7e57ab1348a9e09394da8a602b58de
AdhamG <adhamgaweesh@gmail.com>
2025-10-18 21:59:32 +0300

    fixed the file structure

--- src/CustomerProductDatabase.java
+++ src/CustomerProductDatabase.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package customerproduct;
 
 import java.io.File;
 import java.io.FileNotFoundException;

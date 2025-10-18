a550843747e347a8a2bcb6f05a034536d76e0382
AdhamG <adhamgaweesh@gmail.com>
2025-10-18 21:32:33 +0300

    --Employee-- Added missing getters to other variables as well as some comments.
    --Database--
    Added comments, a new getter for filename, a new function to directly add a new record from string to make that process easier to write and reorganized the class.

--- src/EmployeeUser.java
+++ src/EmployeeUser.java
@@ -1,5 +1,8 @@
 public class EmployeeUser {
+    // all variables are final
     private final String employeeId, name, email, address, phoneNumber;
+
+    // init function
     public EmployeeUser(String employeeId, String name, String email,
                         String address, String phoneNumber){
         this.employeeId = employeeId;
@@ -9,11 +12,28 @@ public class EmployeeUser {
         this.phoneNumber = phoneNumber;
     }
 
+    // returns a serialized representation of the employee object
     public String lineRepresentation(){
         return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
     }
 
     public String getSearchKey(){
         return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
--- src/EmployeeUserDatabase.java
+++ src/EmployeeUserDatabase.java
@@ -6,13 +6,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeUserDatabase {
+    // all variables are final as they will not change
     private final ArrayList<EmployeeUser> records = new ArrayList<>();
     private final String filename;
 
+    // init function, also calls readFromFile so that data is automatically loaded on creation
     public EmployeeUserDatabase(String filename){
         this.filename = filename;
+        readFromFile();
     }
 
+    // file reading and writing
     public void readFromFile(){
         try (Scanner reader = new Scanner(new File(filename))) {
             while (reader.hasNextLine()) {
@@ -25,6 +29,21 @@ public class EmployeeUserDatabase {
         }
     }
 
+    public void saveToFile(){
+        StringBuilder text = new StringBuilder();
+        for (EmployeeUser emp: records){
+            text.append(emp.lineRepresentation());
+            text.append("\n");
+        }
+        try(FileWriter writer = new FileWriter(filename)){
+            writer.write(text.toString());
+        } catch (IOException e) {
+            System.err.println("Error writing to file:" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // record creation, deletion and searching
     public EmployeeUser createRecordFrom(String line){
         String[] parts = line.split(",");
         if (parts.length < 5) {
@@ -33,12 +52,25 @@ public class EmployeeUserDatabase {
         return new EmployeeUser(parts[0],parts[1],parts[2],parts[3],parts[4]);
     }
 
-    public ArrayList<EmployeeUser> returnAllRecords(){
-        return records;
+    public void insertRecord(EmployeeUser record){
+        if (!contains(record.getSearchKey())) {
+            records.add(record);
+        }else {
+            throw new IllegalArgumentException("Employee ID must be a unique string.");
+        }
     }
 
-    public boolean contains(String key){
-        return getRecord(key) != null;
+    public void insertNewRecord(String line){
+        insertRecord(createRecordFrom(line));
+    }
+
+    public void deleteRecord(String key){
+        EmployeeUser temp = getRecord(key);
+        if (temp != null){
+            records.remove(temp);
+        }else {
+            throw new IllegalArgumentException("Given ID does not exist in records.");
+        }
     }
 
     public EmployeeUser getRecord(String key){
@@ -50,29 +82,16 @@ public class EmployeeUserDatabase {
         return null;
     }
 
-    public void insertRecord(EmployeeUser record){
-        if (!contains(record.getSearchKey())) {
-            records.add(record);
-        }else {
-            throw new IllegalArgumentException("Employee ID must be a unique string");
-        }
+    public boolean contains(String key){
+        return getRecord(key) != null;
     }
 
-    public void deleteRecord(String key){
-        records.removeIf(emp -> key.equals(emp.getSearchKey()));
+    // getters
+    public ArrayList<EmployeeUser> returnAllRecords(){
+        return records;
     }
 
-    public void saveToFile(){
-        StringBuilder text = new StringBuilder();
-        for (EmployeeUser emp: records){
-            text.append(emp.lineRepresentation());
-            text.append("\n");
-        }
-        try(FileWriter writer = new FileWriter(new File(filename))){
-            writer.write(text.toString());
-        } catch (IOException e) {
-            System.err.println("Error writing to file:" + e.getMessage());
-            e.printStackTrace();
-        }
+    public String getFilename(){
+        return filename;
     }
 }

c37c35489a04c6dbb457d8e3fc8de675c8597024
unknown <yassine.waleed@gmail.com>
2025-10-19 03:25:41 +0300

    All Roles Done

--- LabFourRoles/src/roles/AdminRole.java
+++ LabFourRoles/src/roles/AdminRole.java
@@ -9,5 +9,33 @@ package roles;
  * @author HY
  */
 public class AdminRole {
+    private EmployeeUserDatabase database;
     
+    public AdminRole() {
+        database = new EmployeeUserDatabase("Employees.txt");
+        database.readFromFile();
+    }
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        EmployeeUser employeeToAdd = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+        database.insertRecord(employeeToAdd);
+        database.saveToFile();
+    }
+    
+    
+    public void removeEmployee(String ID_key) {
+        if (database.contains(ID_key)) {
+            database.deleteRecord(ID_key);
+            database.saveToFile();
+        }
+        }
+    public EmployeeUser[] getListOfEmployees() {
+        
+        return database.returnAllRecords().toArray(new EmployeeUser[database.returnAllRecords().size()]);
+
+    }
+    public void logout() {
+        database.saveToFile();
+        System.out.println("Logged Out");
+    }
+
 }
--- LabFourRoles/src/roles/EmployeeRole.java
+++ LabFourRoles/src/roles/EmployeeRole.java
@@ -4,10 +4,121 @@
  */
 package roles;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /**
  *
  * @author HY
  */
 public class EmployeeRole {
+
+    private ProductDatabase  = productsDatabase;
+    private CustomerProductDatabase  = customerProductDatabase;
+
+    public EmployeeRole() {
+        productsDatabase = new ProductDatabase("Products.txt");
+        productsDatabase.readFromFile();
+
+        customerProductDatabase = new CustomerProductDatabase("CustomersProducts.txt");
+        customerProductDatabase.readFromFile();
+    }
+
+
+    public void SetProductDatabase(ProductDatabase A_productDatabase) {
+        this.productsDatabase = A_productDatabase;
+    }
+
+    public ProductDatabase GetProductDatabase() {
+        return productsDatabase;
+    }
+
+    public void SetCustomerProductDatabase(CustomerProductDatabase A_CustomerProductDatabase) {
+        this.customerProductDatabase = A_CustomerProductDatabase;
+    }
     
+    public CustomerProductDatabase GetCustomerProductDatabase() {
+        return customerProductDatabase;
+    }
+
+    public void addProduct(String productID, String productName, String manufacturerName, String supplierName, int quantity) {
+        Product productedAdded = new Product(String productID
+        , String productName, String manufacturerName
+        , String supplierName, int quantity
+        );
+        productsDatabase.addProduct(productedAdded);
+        productsDatabase.saveToFile("Products.txt");
+    }
+
+    public Product[] getListOfProducts() {
+        return productsDatabase.loadFromFile("Products.txt");
+    }
+
+    public CustomerProduct[] getListOfPurchasingOperations() {
+        return customerProductDatabase.loadFromFile("CustomersProducts.txt");
+    }
+
+    public boolean purchaseProduct(String customerSSN, StringproductID, LocalDate purchaseDate) {
+        Product productToCheck = productsDatabase.findProductById(productID);
+        if (productToCheck != null && productToCheck.getQuantity() != 0) {
+
+            productToCheck.setQuantity(productToCheck.getQuantity() - 1);
+            productsDatabase.saveToFile("Products.txt");
+
+            CustomerProduct createPurchase = new CustomerProductString customerSSN
+            , String productID, LocalDate, purchaseDate
+            );
+        customerProductDatabase.addPurchase(createPurchase);
+            customerProductDatabase.saveToFile("CustomersProducts.txt");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double returnProduct(String customerSSN, String productID, LocalDate purchaseDate, LocalDate returnDate) {
+
+        Product productToReturn = productsDatabase.getRecord(productID);
+
+        if (returnDate.isBefore(purchaseDate) || (ChronoUnit.DAYS.between(purchaseDate, returnDate)) > 14) {
+            return -1;
+        }
+
+        if (productToReturn == null) {
+            return -1;
+        }
+
+        if (!customerProductDatabase.contains(customerSSN + "," + productID + "," + String.format("%02d-%02d-%04d", purchaseDate.getDayOfMonth(), purchaseDate.getMonthValue(), purchaseDate.getYear()))) {
+            return -1;
+        }
+
+        productToReturn.setQuantity(productToReturn.getQuantity() + 1);
+        productsDatabase.saveToFile();
+
+        customerProductDatabase.deleteRecord(customerSSN + "," + productID + "," + String.format("%02d-%02d-%04d", purchaseDate.getDayOfMonth(), purchaseDate.getMonthValue(), purchaseDate.getYear()));
+        customerProductDatabase.saveToFile();
+
+        return productToReturn.getPrice();
+    }
+
+    public void logout() {
+        productsDatabase.saveToFile("Products.txt");
+        customerProductDatabase.saveToFile("CustomersProducts.txt");
+        System.out.println("All unsaved data,Are saved now.");
+    }
+
+    public boolean applyPayment(String customerSSN, LocalDate purchaseDate) {
+        String formattedDate = String.format("%02d-%02d-%04d", purchaseDate.getDayOfMonth(), purchaseDate.getMonthValue(), purchaseDate.getYear());
+
+        CustomerProduct transaction = customerProductDatabase.getRecord(customerSSN + ",," + formattedDate);
+
+        if (transaction == null || transaction.isPaid()) {
+            return false;
+        } else {
+            transaction.setPaid(true);
+            customerProductDatabase.saveToFile();
+            return true;
+        }
+    }
+
 }
